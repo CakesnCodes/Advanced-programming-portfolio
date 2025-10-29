798d1bbe7d20c84e89de8574bec3f3f876646155
CakesnCodes <scribblechronicles@protonmail.com>
2025-10-29 14:59:42 +0000

    Add files via upload

---
+++ Exercise - 1 Math Quiz.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 23 21:02:12 2025
+
+@author: User
+"""
+import random
+
+def decideOperation():
+    operate = ["+","-"]
+    toggle = random.choice(operate)
+    return toggle
+
+def IsCorrect(ans,guess):
+    if ans == guess: return True
+    else: return False
+
+def randomInt(a):
+    num1 = a[0]
+    num2 = a[1]
+    num = random.randint(num1,num2)
+    return num
+
+def displayResults(points):
+    if points > 90: rank = "S"
+    elif points <= 90 and points > 80: 
+        rank = "A"
+        print("Well done!")
+    elif points <= 80 and points > 70: rank = "B"
+    elif points <= 70 and points > 60: rank = "C"
+    elif points <= 60 and points > 50: rank = "D"
+    else: rank = "F"
+    
+    print(f"\n\nYour score is {points}/100,\nYou have gained a rank of {rank}!")
+    if rank == "F":
+        print("Yikes!")
+        
+
+diff = {"easy": (0,9),
+        "medium":(10,99),
+        "hard":(1000,9999)}
+
+play = ""
+
+while play != "N":
+    print("Calculester: QUIZ EDITION")
+    start = input("DIFFICULTY LEVEL \n1. Easy \n2. Moderate \n3. Advanced \n\n " ).lower()
+    a = diff.get(start)
+    points = 0
+    for i in range(10):
+        x = randomInt(a) 
+        y = randomInt(a)
+        op = decideOperation()
+        ans = lambda x, y, op: x + y if op == "+" else x - y
+        que = lambda op: "plus" if op == "+" else "minus"
+        for tries in range(2):
+            try: 
+                guess = int(input(f"{i+1}) what is {x} {que(op)} {y}? ")) #question
+                correct = IsCorrect(ans(x,y,op),guess) # sends it to correct function
+                if correct == True and tries == 0: # if correct on first try
+                    print("That's correct! you receive 10 points")
+                    points += 10
+                    break
+                elif correct == True and tries == 1:#if correct on second try
+                    print("That's correct! you receive 5 points")
+                    points += 5
+                    break
+                elif correct == False and tries == 0: 
+                    print("Oops! let's try that again")
+                else: 
+                    print(f"Oops! the answer is {ans(x,y,op)} better luck next time!")
+                    break
+            except ValueError: 
+                print("Whoops that's not a number! ")
+    displayResults(points)
+    play = input("Wanna play again? [Y/N]\n\n").upper()
+print("Goodbye!")
+
+
---
+++ Exercise 2 - Alexa, Tell me a joke.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  5 13:38:39 2025
+
+@author: User
+"""
+import random
+from tkinter import * 
+from tkinter import filedialog
+root = Tk()
+
+root.geometry("350x200")
+root.resizable(0,0)
+
+        
+def rng():
+    global num
+    num= random.randrange(0, 36)
+    if num % 2 != 0:
+        num -= 1
+        jokes(num)
+    else:
+        jokes(num)
+
+def jokes(num):
+    with open("randomJokes.txt", errors="ignore") as setup:
+        joke = setup.readlines()
+        l1.config(text= f"{joke[num]}")
+        l2.config(text="")
+
+        global why
+        why = Button(root,text="  ?  ",command=cont,font=("Comic Sans MS",10))
+        why.pack(pady=5, padx=5)
+        ask.config()
+        
+def cont():
+    with open("randomJokes.txt", errors="ignore") as setup:
+        punch = setup.readlines()
+        l2.config(text= f"{punch[num+1]}") 
+        why.pack_forget()
+
+
+l1 = Label(root,text=" ")
+l1.pack(pady=15)
+
+l2 = Label(root,text=" ")
+l2.pack(pady=5,padx=5)
+    
+
+ask = Button(root,text="Alexa, tell me a joke",command=rng,font=("Comic Sans MS",10))
+ask.pack(pady=10)
+
+
+
+root.mainloop() #Opens application
+
